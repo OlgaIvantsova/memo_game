a76428724028e4abc473f9c2a4767e0010ee27f1
OlgaIvantsova <sigyn9000@gmail.com>
2016-03-25 16:18:46 +0200

    timer and page reload

--- js/script.js
+++ js/script.js
@@ -12,38 +12,63 @@ $(document).ready(function () {
     	success: function (data) { 
     		fieldSize = data;
 			imagesAmount = fieldSize.width*fieldSize.height;
-			if(imagesAmount>64) {imagesAmount=64};
-			console.log(fieldSize);
+			if(imagesAmount>64) {imagesAmount=64};//define size of game field
+			console.log(fieldSize);	
 		}  
  	});
- 	addImages();
- 	loadImages(imagesAmount);
+ 	
  	createGameField();
- 	addImages();
+ 	loadImages(imagesAmount);//load images from server
+ 	addImages(); //add images on page
+ 	
+ 	
  	// console.log(images);	
  	// alert(1); 
 
- 	var compareArr=[];
-
+ 	var compareArr=[];//array for compare opened images
+    var timer;
  	$('.image-wrap').click(function(event) {
  		event.preventDefault();
+
+ 		if (!timer) { // startTimer
+			var sec = 0;
+			var timerWrap = document.getElementById('timer');
+			timer = setInterval(function() {
+				sec++;
+				timerWrap.innerHTML = sec + 'сек';
+			}, 1000);
+		} 
  		var imageWrap = $(this);
- 		if(imageWrap.hasClass('image-wrap-active')) return;
+ 		if(imageWrap.hasClass('image-wrap-active')) return;//only 2 images can be open
  		imageWrap.addClass('image-wrap-active');
- 		imageWrap.find('.image-shadow').hide();
- 		// if(!($('div').is('.image-wrap'))) {alert("Ты выиграл!");}
- 		
+ 		imageWrap.find('.image-shadow').hide();//open image
+ 		if($('.image-wrap-active').length==imagesAmount) {
+ 			clearInterval(timer);
+ 			(function () {
+ 				var timerWrap = document.getElementById('timer');
+ 				var result = parseInt(timerWrap.innerHTML);
+ 				timerWrap.innerHTML = 'Поздравляем! Твой результат составил ' +result+ ' сек';
+ 				var button = document.createElement('div');
+ 				button.id = "button";
+ 				button.innerHTML = "Новая игра"; 
+ 				document.body.insertBefore(button, document.body.lastChild);
+ 				$('#button').on('click', function () {
+					location.reload();
+		
+			});
+ 			})();
+ 			// compareArr.length = 0;
+ 			// return;
+ 		}
  		compareArr.push(imageWrap);
- 		console.log($('.image-wrap').length);
  		if (compareArr.length==3)
  		{			
- 			if (compareArr[0].attr('id') == compareArr[1].attr('id')) {
+ 			if (compareArr[0].attr('id') == compareArr[1].attr('id')) {//if pictures match, close them
  				for (var i = 0; i < compareArr.length-1; i++) {
  					compareArr[i].removeClass('image-wrap').addClass('image-wrap-hidden');
  					compareArr[i].find('.image-shadow').css({background:'#ccc'}).show();
  				};
  				compareArr[compareArr.length-1].removeClass('image-wrap-active').find('.image-shadow').show();
- 				if($('.image-wrap').length==2) {alert("Ты выиграл!");}
  				compareArr.length = 0;
  			} else {
  				for (var i = 0; i < compareArr.length; i++) {
@@ -55,10 +80,18 @@ $(document).ready(function () {
  		}
  		
  	});            
-
-
-		
+	
+			
 });
+
+function startTimer (timer) {
+		var sec = 0;
+			var timerWrap = document.getElementById('timer');
+			timer = setInterval(function() {
+				sec++;
+				timerWrap.innerHTML = sec + 'сек';
+			}, 1000);
+}
 
 function random(min, max) {
 	var rand = min + Math.random() * (max + 1 - min);
@@ -83,37 +116,40 @@ function loadImages(amount) {
 				}
 			}
 			images = images.concat(images);
-			
 			// var randomArr = images.slice();
 			images = images.slice().map(function() {
 				return images.splice(random(0,images.length-1), 1)[0];
 				// randomArr.unshift(element1)
 				// return arr[random(i, images.length-1)];
 			});
-			console.log(images);
+			// console.log(images);
 		}  
  	});
 }
 
 function createGameField() {
-	var IMAGE_LENGTH = 96;
+	var IMAGE_LENGTH = 70;
 	var wrapper = document.createElement('div');
 	var wrapWidth = fieldSize.width*IMAGE_LENGTH;
 	wrapper.style.width = wrapWidth +'px';
 	wrapper.id = 'game-wrap';
 	wrapper.className = "clearfix";
 	document.body.appendChild(wrapper);
+	var timerWrap = document.createElement('div');
+			timerWrap.id = "timer";
+		    timerWrap.innerHTML = '0 сек';
+			document.body.insertBefore(timerWrap, document.body.firstChild);
 }
 
 function addImages() {
 	var gameWrap = document.getElementById('game-wrap');
 	for (var i = 0; i < images.length; i++) {
-		var elem = createImagesTable(images, i);
+		var elem = createImagesGrid(images, i);
 		gameWrap.appendChild(elem);
 	};
 	return gameWrap;
 }
-function createImagesTable(arr, i) {
+function createImagesGrid(arr, i) {
 		var imageWrap = document.createElement('div');
 		imageWrap.className = 'image-wrap';
 		var image = document.createElement('img');
